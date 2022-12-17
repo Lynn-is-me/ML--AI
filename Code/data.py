8b712b5f426b868ee6752cfb5d073de17165fef6
Lynnnnn <112313591+Lynnislearninghowtocode@users.noreply.github.com>
2022-12-17 19:40:42 +0700

    Update data.py

--- Code/data.py
+++ Code/data.py
@@ -4,8 +4,8 @@ def load_data(filename):
     df = pd.read_csv(filename)
     df_binary = df[['Salnty', 'T_degC']]
     
-    # Taking only the selected two attributes from the dataset
+
     df_binary.columns = ['Sal', 'Temp']
-    #display the first 5 rows
+
     df_binary.head()
     print(df)
