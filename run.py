859b66d1fb21db38fa7f2238c18e952447854ad7
Lynnnnn <112313591+Lynnislearninghowtocode@users.noreply.github.com>
2022-12-17 19:19:16 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def load_data(filename):
+    df = pd.read_csv(filename)
+    df_binary = df[['Salnty', 'T_degC']]
+    
+    # Taking only the selected two attributes from the dataset
+    df_binary.columns = ['Sal', 'Temp']
+    #display the first 5 rows
+    df_binary.head()
+    print(df)
+    
+if __name__=="__main__":
+    load_data("bottle.csv")
+    
